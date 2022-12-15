2213d75a9512f0f18686ffe96265349687498b9a
unknown <70806774+vcecan@users.noreply.github.com>
2022-12-15 14:30:30 +0200

    sec commit

--- idweb2022/Controllers/HomeController.cs
+++ idweb2022/Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace idweb2022.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }).
         }
     }
 }
